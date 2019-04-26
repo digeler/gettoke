532971284fee9b3a8164ef3d7e18389e32aca069
Dinor Geler <digeler@users.noreply.github.com>
2019-04-26 10:12:58 +0300

    file

---
+++ acquiretoke.ps1
@@ -0,0 +1,4 @@
+Import-Module ./gettoke.psm1
+$t = Get-AuthenticationResult
+$t.AccessToken > token.txt
+Write-Host "Your token is in file name token.txt ,copy this file and set the env varialble token=token context"
